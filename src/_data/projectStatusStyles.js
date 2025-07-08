89f4b69bf24e3463b2b87b9af10835322094ffcb
Gene Marks <genemarks74@gmail.com>
2025-07-08 10:53:40 -0400

    Add shared data for project status css class map

---
+++ src/_data/projectStatusStyles.js
@@ -0,0 +1,7 @@
+export default {
+    active: "border-1 border-[#304200] text-[#c7df86] bg-[#507312]",
+    maintenance: "border-1 border-[#12398a] text-[#2a3a77] bg-[#d6dff2]",
+    refactoring: "border-2 border-dashed border-black text-black bg-[#f2d800]",
+    archived: "border-1 border-dotted border-secondary text-zinc-600 bg-zinc-200",
+    default: "border-1"
+}
