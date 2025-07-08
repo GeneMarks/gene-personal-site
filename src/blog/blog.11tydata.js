91410e296c0c765e1a2ce7ad9e5b4d69777ae6a1
Gene Marks <genemarks74@gmail.com>
2025-07-08 10:53:04 -0400

    Add blog shared data

---
+++ src/blog/blog.11tydata.js
@@ -0,0 +1,10 @@
+export default {
+    layout: "layouts/post.njk",
+    tags: ["post"],
+    eleventyComputed: {
+        date: data => {
+            const match = data.page.fileSlug.match(/^(\d{4}-\d{2}-\d{2})/);
+            return match ? new Date(match[1]) : new Date();
+        },
+    },
+};
