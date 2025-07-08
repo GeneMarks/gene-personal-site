cac4a330b9822060bc782160472337ee58535234
Gene Marks <genemarks74@gmail.com>
2025-07-08 15:02:59 -0400

    Implement filter to print date, delete buildDate data file

--- eleventy.config.js
+++ eleventy.config.js
@@ -11,4 +11,21 @@ export default function(config) {
     config.addPassthroughCopy("src/assets/fonts/**/*");
     config.addPassthroughCopy("src/assets/images/**/*");
     config.addPassthroughCopy("src/assets/js/**/*");
+
+    // Filters
+    config.addFilter("printDate", (value) => {
+        const formatted = new Intl.DateTimeFormat("en-US", {
+            timeZone: "America/New_York",
+            weekday: "short",
+            year: "numeric",
+            month: "short",
+            day: "2-digit",
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+            timeZoneName: "short"
+        }).format(value);
+
+        return formatted;
+    });
 };
