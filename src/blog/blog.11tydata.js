91f48ea8d6bddfb0b093c054fe99baffee7000e9
Gene Marks <genemarks74@gmail.com>
2025-07-08 15:35:04 -0400

    Remove unnecessary computed data

--- src/blog/blog.11tydata.js
+++ src/blog/blog.11tydata.js
@@ -1,10 +1,4 @@
 export default {
     layout: "layouts/post.html",
-    tags: ["post"],
-    eleventyComputed: {
-        date: data => {
-            const match = data.page.fileSlug.match(/^(\d{4}-\d{2}-\d{2})/);
-            return match ? new Date(match[1]) : new Date();
-        },
-    },
+    tags: ["post"]
 };
