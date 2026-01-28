8c5360c7d10488ca8d64178c24b322fe4175f4ca
Gene Marks <genemarks74@gmail.com>
2026-01-28 15:17:32 -0500

    Change "5.gif" margin to -2

--- src/assets/js/sprite-loader.js
+++ src/assets/js/sprite-loader.js
@@ -1,9 +1,9 @@
-const marginMap = {
+const margins = {
     "1.gif": -5,
     "2.gif": -5,
     "3.gif": -2,
     "4.gif": -4,
-    "5.gif": -12,
+    "5.gif": -2,
     "6.gif": -6,
     "7.gif": -2,
     "8.gif": -4,
@@ -15,5 +15,5 @@ const filename = `${index}.gif`;
 const path = `/assets/images/gene_sprites/${filename}`;
 
 const sprite = document.getElementById("gene-sprite");
+sprite.style.marginBottom = `${margins[filename]}px`;
 sprite.src = path;
-sprite.style.marginBottom = `${marginMap[filename]}px`;
