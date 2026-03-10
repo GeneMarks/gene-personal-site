eca83effcdf131a0e549cb763f6c1e0679459cf5
Gene Marks <genemarks74@gmail.com>
2026-03-10 12:59:48 -0400

    Add sitePublishedYear to metadata.js

--- src/_data/metadata.js
+++ src/_data/metadata.js
@@ -2,6 +2,7 @@ export default {
     url: "https://blog.genemarks.net",
     siteName: "Gene's Blog",
     siteDesc: "Anything that comes to mind",
+    sitePublishedYear: 2019,
     authorName: "Gene Marks",
     authorEmail: "mail@genemarks.net",
     sourceUrl: "https://github.com/GeneMarks/blog.genemarks.net",
