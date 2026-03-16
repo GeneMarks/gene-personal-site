883cefa5b57febde61ba3c19398c03c25f169569
Gene Marks <genemarks74@gmail.com>
2026-03-16 19:37:02 -0400

    Remove console.log debug line in events.js

--- _11ty/events.js
+++ _11ty/events.js
@@ -3,7 +3,6 @@ import { execSync } from "node:child_process";
 export default async function(config) {
 
     config.on("eleventy.after", ({ directories }) => {
-        console.log(directories.output)
         execSync(`npx -y pagefind --site "${directories.output}" --glob \"**/*.html\"`, { encoding: "utf-8" });
     });
 }
