67248823f9e2dae8896cc9d5e5ec9ac5c62a8ff6
Gene Marks <genemarks74@gmail.com>
2026-03-17 00:37:34 -0400

    Remove support for mermaid-cli generation during eleventy build
    This proved to be too complicated to get working in Docker with
    mermaid-cli/puppeteer. It is simpler to use a custom script
    and manually build charts outside of the eleventy build process.
    
    `npm run mermaid` can now be used to execute the script.

--- _11ty/events.js
+++ _11ty/events.js
@@ -1,6 +1,4 @@
 import { execSync } from "node:child_process";
-import * as fs from "node:fs";
-import * as path from "node:path";
 
 export default async function(config) {
 
@@ -8,23 +6,4 @@ export default async function(config) {
         execSync(`npx -y pagefind --site "${directories.output}" --glob \"**/*.html\"`, { encoding: "utf-8" });
     });
 
-    config.on("eleventy.after", ( { directories } ) => {
-        const mermaidsInputPath = `${directories.input}/_mermaid`;
-        const mermaidsOutputPath = `${directories.output}/uploads/mermaid`;
-
-        if (!fs.existsSync(mermaidsOutputPath)) {
-            fs.mkdirSync(mermaidsOutputPath);
-        }
-
-        const mermaids = fs.readdirSync(mermaidsInputPath);
-        mermaids.forEach((file, _) => {
-            const inputPath = path.join(mermaidsInputPath, file);
-            const inputExtension = path.extname(file);
-            const inputName = path.basename(file, inputExtension);
-
-            const outputPath = path.join(mermaidsOutputPath, `${inputName}.svg`);
-
-            execSync(`npx -p @mermaid-js/mermaid-cli mmdc -p "puppeteer-config.json" -i "${inputPath}" -o "${outputPath}" -b transparent`);
-        });
-    });
 }
---
+++ scripts/generate-mermaid.js
@@ -0,0 +1,21 @@
+import * as fs from "node:fs";
+import * as path from "node:path";
+import { execSync } from "node:child_process";
+
+const mermaidsInputPath = "./src/_mermaid";
+const mermaidsOutputPath = "./src/uploads/mermaid";
+
+if (!fs.existsSync(mermaidsOutputPath)) {
+    fs.mkdirSync(mermaidsOutputPath);
+}
+
+const mermaids = fs.readdirSync(mermaidsInputPath);
+mermaids.forEach((file, _) => {
+    const inputPath = path.join(mermaidsInputPath, file);
+    const inputExtension = path.extname(file);
+    const inputName = path.basename(file, inputExtension);
+
+    const outputPath = path.join(mermaidsOutputPath, `${inputName}.svg`);
+
+    execSync(`npx -p @mermaid-js/mermaid-cli mmdc -i "${inputPath}" -o "${outputPath}" -b transparent`, { stdio: "inherit" });
+});
